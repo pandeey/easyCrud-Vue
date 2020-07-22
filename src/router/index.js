c0cba9344b4e160ba917b8be8704c8311836f3d2
eddy <eddyjo@163.com>
2020-07-22 23:15:00 +0800

    [ADD] 新增表单

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI
+)
 Vue.use(Router)
 
 export default new Router({
